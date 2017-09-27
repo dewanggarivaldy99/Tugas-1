f9d8bed0cb9b9ffac0ef55fc1dff73925cdccb35
dewanggarivaldy99 <32315980+dewanggarivaldy99@users.noreply.github.com>
2017-09-27 08:19:01 +0700

    04217007.py
    Tugas algoritma dan pemrograman

---
+++ tugas python 1.py
@@ -0,0 +1,9 @@
+nama = "Dewangga Rivaldy Pratama"
+nim = "04217007"
+umur = 18
+print ("namaku :"+ nama)
+print ("nimku :" +nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5)
